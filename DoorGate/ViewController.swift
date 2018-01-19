3de895b81fc8f227a330cf7584484f54c6c34fc3
Yuliani Noriega <yuliani.noriega@ynoriega-09.local>
2018-01-19 11:37:08 -0500

    init commit

---
+++ DoorGate/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  DoorGate
+//
+//  Created by Yuliani Noriega on 1/19/18.
+//  Copyright © 2018 Yuliani Noriega. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
