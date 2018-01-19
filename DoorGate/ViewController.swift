a39e8192b5a174a13153d40f83a11888a17c69a8
Yuliani Noriega <yuliani.noriega@ynoriega-09.local>
2018-01-19 14:26:42 -0500

    DoorGate implemented without RXSwift

--- DoorGate/ViewController.swift
+++ DoorGate/ViewController.swift
@@ -8,19 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum State:Int{
+    case Open = 0, Occupied, Locked
+}
 
+let stateString = ["Open", "Occupied", "Locked"]
+
+class ViewController: UIViewController {
+    //Labels
+    @IBOutlet weak var stateLabel: UILabel!
+    @IBOutlet weak var countLabel: UILabel!
+    //Buttons
+    @IBOutlet weak var inButton: UIButton!
+    @IBOutlet weak var outButton: UIButton!
+    //State Variable
+    var doorState = State.Open.rawValue
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-
+        changeState(state: 0)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //Mark: Button Action
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        var state = 1
+        if sender == outButton {
+            state = -1
+        }
+        changeState(state: state)
     }
-
-
+    
+    func changeState(state:Int) {
+        //updating state
+        doorState = doorState + state
+        
+        //updating UI
+        countLabel.text = String(doorState)
+        stateLabel.text = stateString[doorState]
+        
+        switch doorState {
+        case State.Open.rawValue:
+            inButton.isEnabled = true
+            outButton.isEnabled = false
+            return
+        case State.Occupied.rawValue:
+            inButton.isEnabled = true
+            outButton.isEnabled = true
+            return
+        case State.Locked.rawValue:
+            inButton.isEnabled = false
+            outButton.isEnabled = true
+            return
+        default:
+            return
+        }
+    }
 }
 
