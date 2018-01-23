1726985c648c085b040c3dc9fef78479a08c144d
Yuliani Noriega <yuliani.noriega@ynoriega-09.local>
2018-01-23 15:38:15 -0500

    Project ported to RXSwift Paradigm

--- DoorGate/ViewController.swift
+++ DoorGate/ViewController.swift
@@ -7,10 +7,8 @@
 //
 
 import UIKit
-
-enum State:Int{
-    case Open = 0, Occupied, Locked
-}
+import RxSwift
+import RxCocoa
 
 let stateString = ["Open", "Occupied", "Locked"]
 
@@ -21,47 +19,44 @@ class ViewController: UIViewController {
     //Buttons
     @IBOutlet weak var inButton: UIButton!
     @IBOutlet weak var outButton: UIButton!
-    //State Variable
-    var doorState = State.Open.rawValue
+    //Dispose Bag
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        changeState(state: 0)
-    }
-
-    //Mark: Button Action
-    @IBAction func buttonPressed(_ sender: UIButton) {
-        var state = 1
-        if sender == outButton {
-            state = -1
-        }
-        changeState(state: state)
+        
+        //In Button Logic
+        inButton.rx.tap.scan(0) { (priorValue, _) in
+                return 1
+            }
+            .asDriver(onErrorJustReturn: 0)
+            .drive(onNext: { [unowned self] currentCount in
+                let count = Int(self.countLabel.text!)! + currentCount
+                //UI Changes
+                self.buttonLogic(count: count)
+                self.countLabel.text = String(count)
+                self.stateLabel.text = stateString[count]
+            })
+            .disposed(by: disposeBag)
+       
+        //Out Button Logic
+        outButton.rx.tap.scan(0) { (priorValue, _) in
+                return -1
+            }
+            .asDriver(onErrorJustReturn: 0)
+            .drive(onNext: { [unowned self] currentCount in
+                let count = Int(self.countLabel.text!)! + currentCount
+                //UI Changes
+                self.buttonLogic(count: count)
+                self.countLabel.text = String(count)
+                self.stateLabel.text = stateString[count]
+            })
+            .disposed(by: disposeBag)
     }
     
-    func changeState(state:Int) {
-        //updating state
-        doorState = doorState + state
-        
-        //updating UI
-        countLabel.text = String(doorState)
-        stateLabel.text = stateString[doorState]
-        
-        switch doorState {
-        case State.Open.rawValue:
-            inButton.isEnabled = true
-            outButton.isEnabled = false
-            return
-        case State.Occupied.rawValue:
-            inButton.isEnabled = true
-            outButton.isEnabled = true
-            return
-        case State.Locked.rawValue:
-            inButton.isEnabled = false
-            outButton.isEnabled = true
-            return
-        default:
-            return
-        }
+    func buttonLogic(count :Int) {
+        self.inButton.isEnabled = (count < 2)
+        self.outButton.isEnabled = (count > 0)
     }
 }
 
